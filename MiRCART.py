fcc4215b6826160dad2ec624c1a329d1de028542
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2018-01-06 02:50:12 +0100

    {IrcClient,IrcMiRCARTBot,MiRC{2png,ART}}.py: fix spaces & typo.

--- MiRCART.py
+++ MiRCART.py
@@ -2,17 +2,17 @@
 #
 # MiRCART.py -- mIRC art editor for Windows & Linux
 # Copyright (c) 2018 Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -97,7 +97,7 @@ class MiRCARTCanvas(wx.Panel):
                     self._drawPatch(patch, eventDc, tmpDc, atPoint)
                 self.parentFrame.onCanvasUpdate()
     # }}}
-    # {{{ _pushUndo(self, atPoint, patch): XXXX
+    # {{{ _pushUndo(self, atPoint, patch): XXX
     def _pushUndo(self, atPoint, patch, mapItem):
         if self.patchesUndoLevel > 0:
             del self.patchesUndo[0:self.patchesUndoLevel]
