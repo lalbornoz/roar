a58781ed7a2d2d33b9e9b6f673e6b318c0234739
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2018-01-07 04:19:38 +0100

    MiRCARTCanvasStore.py: split & merge MiRCART{FromTextFile,ToPastebin,ToTextFile}.py.

--- MiRCARTCanvasStore.py
+++ MiRCARTCanvasStore.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# MiRCARTFromTextFile.py -- XXX
+# MiRCARTCanvasStore.py -- XXX
 # Copyright (c) 2018 Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -22,9 +22,26 @@
 # SOFTWARE.
 #
 
-class MiRCARTFromTextFile():
+import base64
+import io
+import wx
+
+try:
+    from MiRCARTToPngFile import MiRCARTToPngFile
+    haveMiRCARTToPngFile = True
+except ImportError:
+    haveMiRCARTToPngFile = False
+
+try:
+    import requests, urllib.request
+    haveUrllib = True
+except ImportError:
+    haveUrllib = False
+
+class MiRCARTCanvasStore():
     """XXX"""
     inFile = inSize = outMap = None
+    parentCanvas = None
 
     #
     # _CellState(): Cell state
@@ -60,8 +77,72 @@ class MiRCARTFromTextFile():
         else:
             return cellState | bit
     # }}}
-    # {{{ _transform(self): XXX
-    def _transform(self):
+
+    # {{{ exportPastebin(self, apiDevKey): XXX
+    def exportPastebin(self, apiDevKey):
+        if haveUrllib:
+            outFile = io.StringIO(); self.exportTextFile(outFile);
+            requestData = {                                                 \
+                "api_dev_key":          self.apiDevKey,                     \
+                "api_option":           "paste",                            \
+                "api_paste_code":       base64.b64encode(outFile.read()),   \
+                "api_paste_name":       pasteName,                          \
+                "api_paste_private":    pastePrivate}
+            responseHttp = requests.post("https://pastebin.com/post.php",   \
+                    data=requestData)
+            if responseHttp.status_code == 200:
+                return responseHttp.text
+            else:
+                return None
+        else:
+            return None
+    # }}}
+    # {{{ exportPngFile(self): XXX
+    def exportPngFile(self, pathName):
+        if haveMiRCARTToPngFile:
+            outFile = io.StringIO(); self.exportTextFile(outFile);
+            MiRCARTToPng(outFile).export(pathName)
+            return True
+        else:
+            return False
+    # }}}
+    # {{{ exportTextFile(self, outFile): XXX
+    def exportTextFile(self, outFile):
+        canvasMap = self.parentCanvas.canvasMap
+        canvasSize = self.parentCanvas.canvasSize
+        for canvasRow in range(0, canvasSize[1]):
+            canvasLastColours = []
+            for canvasCol in range(0, canvasSize[0]):
+                canvasColColours = canvasMap[canvasRow][canvasCol][0:2]
+                canvasColText = self.canvasMap[canvasRow][canvasCol][2]
+            if canvasColColours != canvasLastColours:
+                canvasLastColours = canvasColColours
+                outFile.write("\x03" +          \
+                    str(canvasColColours[0]) +  \
+                    "," + str(canvasColColours[1]))
+                outFile.write(canvasColText)
+            outFile.write("\n")
+    # }}}
+    # {{{ importIntoPanel(self): XXX
+    def importIntoPanel(self):
+        canvasSize = self.inSize; self.parentCanvas.resize(canvasSize);
+        self.parentCanvas.canvasJournal.reset()
+        eventDc = wx.ClientDC(self.parentCanvas); tmpDc = wx.MemoryDC();
+        tmpDc.SelectObject(self.parentCanvas.canvasBitmap)
+        for numRow in range(0, len(self.outMap)):
+            for numCol in range(0, len(self.outMap[numRow])):
+                self.parentCanvas.onJournalUpdate(False,  \
+                    (numCol, numRow), [numCol, numRow,    \
+                    self.outMap[numRow][numCol][0][0],    \
+                    self.outMap[numRow][numCol][0][1],    \
+                    self.outMap[numRow][numCol][2]],      \
+                    eventDc, tmpDc, (0, 0))
+        wx.SafeYield()
+    # }}}
+    # {{{ importTextFile(self, pathName): XXX
+    def importTextFile(self, pathName):
+        self.inFile = open(pathName, "r")
+        self.inSize = self.outMap = None;
         inCurColourSpec = ""; inCurRow = -1;
         inLine = self.inFile.readline()
         inSize = [0, 0]; outMap = []; inMaxCols = 0;
@@ -125,23 +206,38 @@ class MiRCARTFromTextFile():
             inMaxCols = max(inMaxCols, inRowCols)
             inLine = self.inFile.readline()
         inSize[0] = inMaxCols; self.inSize = inSize; self.outMap = outMap;
+        self.inFile.close()
     # }}}
-    # {{{ getMap(self): XXX
-    def getMap(self):
-        if self.outMap == None:
-            self._transform()
-        return self.outMap
-    # }}}
-    # {{{ getSize(self): XXX
-    def getSize(self):
-        if self.inSize == None:
-            self._transform()
-        return self.inSize
+    # {{{ importNew(self, newCanvasSize=None): XXX
+    def importNew(self, newCanvasSize=None):
+        if newCanvasSize != None:
+            self.parentCanvas.resize(newCanvasSize)
+        self.parentCanvas.canvasJournal.reset()
+        self.parentCanvas.canvasMap = [[[1, 1, " "]                             \
+                for x in range(self.parentCanvas.canvasSize[0])]                \
+            for y in range(self.parentCanvas.canvasSize[1])]
+        canvasWinSize = (                                                       \
+            self.parentCanvas.cellSize[0] * self.parentCanvas.canvasSize[0],    \
+            self.parentCanvas.cellSize[1] * self.parentCanvas.canvasSize[1])
+        if self.parentCanvas.canvasBitmap != None:
+            self.parentCanvas.canvasBitmap.Destroy()
+        self.parentCanvas.canvasBitmap = wx.Bitmap(canvasWinSize)
+        eventDc = wx.ClientDC(self.parentCanvas); tmpDc = wx.MemoryDC();
+        tmpDc.SelectObject(self.parentCanvas.canvasBitmap)
+        for numRow in range(0, len(self.parentCanvas.canvasMap)):
+            for numCol in range(0, len(self.parentCanvas.canvasMap[numRow])):
+                self.parentCanvas.onJournalUpdate(False,                        \
+                    (numCol, numRow), [numCol, numRow, 1, 1, " "],              \
+                    eventDc, tmpDc, (0, 0))
+        wx.SafeYield()
     # }}}
 
     #
-    # __init__(self): initialisation method
-    def __init__(self, inFile):
-        self.inFile = inFile; self.inSize = None; self.outMap = None;
+    # __init__(self, inFile=None, parentCanvas=None): initialisation method
+    def __init__(self, inFile=None, parentCanvas=None):
+        self.inFile = inFile; self.inSize = self.outMap = None;
+        self.parentCanvas = parentCanvas
+        if inFile != None:
+            self.importTextFile(inFile)
 
 # vim:expandtab foldmethod=marker sw=4 ts=4 tw=120
