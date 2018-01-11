77eb660f350a095c287ddf647acdd8b1891b3718
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2018-01-11 01:18:50 +0100

    MiRCART.py: import argv[1] into canvas if specified.

--- MiRCART.py
+++ MiRCART.py
@@ -29,7 +29,13 @@ import sys, wx
 # Entry point
 def main(*argv):
     wxApp = wx.App(False)
-    MiRCARTFrame(None)
+    appFrame = MiRCARTFrame(None)
+    if  len(argv) > 1    \
+    and len(argv[1]) > 0:
+        appFrame.panelCanvas.canvasInterface.canvasPathName = argv[1]
+        appFrame.panelCanvas.canvasImportStore.importTextFile(argv[1])
+        appFrame.panelCanvas.canvasImportStore.importIntoPanel()
+        appFrame.onCanvasUpdate(pathName=argv[1], undoLevel=-1)
     wxApp.MainLoop()
 if __name__ == "__main__":
     main(*sys.argv)
