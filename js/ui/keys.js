5d8921b8262a1d17b680eed6e9cb2d5b9d2e0013
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2018-10-25 02:34:47 +0200

    js/ui/keys.js: cleanup.

--- js/ui/keys.js
+++ js/ui/keys.js
@@ -204,33 +204,6 @@ var keys = (function(){
     }
   }
 
-  // function cancelZoom() {
-  //   var d = document,
-  //       viewport,
-  //       content,
-  //       maxScale = ',maximum-scale=',
-  //       maxScaleRegex = /,*maximum\-scale\=\d*\.*\d*/;
-
-  //   // this should be a focusable DOM Element
-  //   if (!this.addEventListener || !d.querySelector) {
-  //     return;
-  //   }
-
-  //   viewport = d.querySelector('meta[name="viewport"]');
-  //   content = viewport.content;
-
-  //   function changeViewport(event) {
-  //     // http://nerd.vasilis.nl/prevent-ios-from-zooming-onfocus/
-  //     viewport.content = content + (event.type == 'blur' ? (content.match(maxScaleRegex, '') ? '' : maxScale + 10) : maxScale + 1);
-  //   }
-
-  //   // We could use DOMFocusIn here, but it's deprecated.
-  //   this.addEventListener('focus', changeViewport, true);
-  //   this.addEventListener('blur', changeViewport, false);
-  // }
-  
-  // cancelZoom.bind(cursor_input)();
-
   return keys
 })()
 
