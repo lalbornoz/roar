5ad678bf4cf77047ee1e531402582b4de6d23271
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2019-09-16 11:15:23 +0200

    Allow pasting text when using text tool.
    libtools/ToolText.py:{_processKeyChar,onKeyboardEvent}(): split from onKeyboardEvent().
    libtools/ToolText.py:onKeyboardEvent(): obtain & iteratively _processKeyChar() clipboard text on <Control> V.
    libtools/ToolText.py:onKeyboardEvent(): dispatch cursor patch once after {drawing,pasting}.
    assets/text/TODO: updated.

--- libtools/ToolText.py
+++ libtools/ToolText.py
@@ -11,24 +11,9 @@ class ToolText(Tool):
     name = "Text"
     rtlRegEx = r'^[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]+$'
 
-    #
-    # onKeyboardEvent(self, atPoint, brushColours, brushPos, brushSize, canvas, dispatchFn, eventDc, keyChar, keyCode, keyModifiers, mapPoint, viewRect)
-    def onKeyboardEvent(self, atPoint, brushColours, brushPos, brushSize, canvas, dispatchFn, eventDc, keyChar, keyCode, keyModifiers, mapPoint, viewRect):
-        if keyCode == wx.WXK_BACK:
-            if brushPos[0] > 0:
-                brushPos[0] -= 1
-            elif brushPos[1] > 0:
-                brushPos[0], brushPos[1] = canvas.size[0] - 1, brushPos[1] - 1
-            else:
-                brushPos[0], brushPos[1] = canvas.size[0] - 1, canvas.size[1] - 1
-            rc, dirty = True, False; dispatchFn(eventDc, True, [*brushPos, *brushColours, 0, "_"], viewRect);
-        elif keyCode == wx.WXK_RETURN:
-            if brushPos[1] < (canvas.size[1] - 1):
-                brushPos[0], brushPos[1] = 0, brushPos[1] + 1
-            else:
-                brushPos[0], brushPos[1] = 0, 0
-            rc, dirty = True, False; dispatchFn(eventDc, True, [*brushPos, *brushColours, 0, "_"], viewRect);
-        elif (ord(keyChar) != wx.WXK_NONE)                          \
+    # {{{ _processKeyChar(self, brushColours, brushPos, canvas, dispatchFn, eventDc, keyChar, keyModifiers, viewRect)
+    def _processKeyChar(self, brushColours, brushPos, canvas, dispatchFn, eventDc, keyChar, keyModifiers, viewRect):
+        if (ord(keyChar) != wx.WXK_NONE)                            \
         and  (not keyChar in set("\t\n\v\f\r"))                     \
         and  ((ord(keyChar) >= 32) if ord(keyChar) < 127 else True) \
         and  (keyModifiers in (wx.MOD_NONE, wx.MOD_SHIFT)):
@@ -47,10 +32,47 @@ class ToolText(Tool):
                     brushPos[0], brushPos[1] = canvas.size[0] - 1, brushPos[1] - 1
                 else:
                     brushPos[0], brushPos[1] = canvas.size[0] - 1, canvas.size[1] - 1
-            dispatchFn(eventDc, True, [*brushPos, *brushColours, 0, "_"], viewRect)
             rc, dirty = True, True
         else:
             rc, dirty = False, False
+        return rc, dirty
+    # }}}
+
+    #
+    # onKeyboardEvent(self, atPoint, brushColours, brushPos, brushSize, canvas, dispatchFn, eventDc, keyChar, keyCode, keyModifiers, mapPoint, viewRect)
+    def onKeyboardEvent(self, atPoint, brushColours, brushPos, brushSize, canvas, dispatchFn, eventDc, keyChar, keyCode, keyModifiers, mapPoint, viewRect):
+        if keyCode == wx.WXK_CONTROL_V:
+            rc, dirty = True, False
+            if  wx.TheClipboard.IsSupported(wx.DataFormat(wx.DF_TEXT))  \
+            and wx.TheClipboard.Open():
+                inBuffer = wx.TextDataObject()
+                if wx.TheClipboard.GetData(inBuffer):
+                    for inBufferChar in list(inBuffer.GetText()):
+                        rc_, dirty_ = self._processKeyChar(brushColours, brushPos, canvas, dispatchFn, eventDc, inBufferChar, 0, viewRect)
+                        rc = True if rc_ else rc; dirty = True if dirty_ else dirty;
+                    if rc:
+                        dispatchFn(eventDc, True, [*brushPos, *brushColours, 0, "_"], viewRect)
+                wx.TheClipboard.Close()
+            else:
+                rc, error = False, "Clipboard does not contain text data and/or cannot be opened"
+        elif keyCode == wx.WXK_BACK:
+            if brushPos[0] > 0:
+                brushPos[0] -= 1
+            elif brushPos[1] > 0:
+                brushPos[0], brushPos[1] = canvas.size[0] - 1, brushPos[1] - 1
+            else:
+                brushPos[0], brushPos[1] = canvas.size[0] - 1, canvas.size[1] - 1
+            rc, dirty = True, False; dispatchFn(eventDc, True, [*brushPos, *brushColours, 0, "_"], viewRect);
+        elif keyCode == wx.WXK_RETURN:
+            if brushPos[1] < (canvas.size[1] - 1):
+                brushPos[0], brushPos[1] = 0, brushPos[1] + 1
+            else:
+                brushPos[0], brushPos[1] = 0, 0
+            rc, dirty = True, False; dispatchFn(eventDc, True, [*brushPos, *brushColours, 0, "_"], viewRect);
+        else:
+            rc, dirty = self._processKeyChar(brushColours, brushPos, canvas, dispatchFn, eventDc, keyChar, keyModifiers, viewRect)
+            if rc:
+                dispatchFn(eventDc, True, [*brushPos, *brushColours, 0, "_"], viewRect)
         return rc, dirty
 
     #
