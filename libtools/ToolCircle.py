b088d6ffe3103cf0f13ffe9c15a459118746912e
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2019-09-26 23:34:01 +0200

    Implement {circle,rectangle} dragging w/ <Ctrl>.
    assets/text/hotkeys.txt: updated.
    assets/text/TODO: updated.

--- libtools/ToolCircle.py
+++ libtools/ToolCircle.py
@@ -5,16 +5,15 @@
 #
 
 from Tool import Tool
+import wx
 
 class ToolCircle(Tool):
     name = "Circle"
+    TS_NONE     = 0
+    TS_ORIGIN   = 1
 
-    def onMouseEvent(self, atPoint, brushColours, brushPos, brushSize, canvas, keyModifiers, mapPoint, mouseDragging, mouseLeftDown, mouseRightDown):
-        brushColours, brushSize, isCursor = list(brushColours), [brushSize[0] * 2, brushSize[1]], not (mouseLeftDown or mouseRightDown)
-        originPoint, radius = (brushSize[0] / 2, brushSize[0] / 2), brushSize[0]
-        if mouseRightDown:
-            brushColours = [brushColours[1], brushColours[0]]
-        cells = []
+    def _drawCircle(self, brushColours, canvas, mapPoint, originPoint, radius):
+        cells, patches = [], []
         for brushY in range(-radius, radius + 1):
             cells += [[]]
             for brushX in range(-radius, radius + 1):
@@ -22,7 +21,6 @@ class ToolCircle(Tool):
                     cells[-1] += [[mapPoint[i] + int(originPoint[i] + o) for i, o in zip((0, 1,), (brushX, brushY,))]]
             if cells[-1] == []:
                 del cells[-1]
-        patches = []
         for numRow in range(len(cells)):
             for numCol in range(len(cells[numRow])):
                 if ((numRow == 0) or (numRow == (len(cells) - 1)))                              \
@@ -43,6 +41,31 @@ class ToolCircle(Tool):
                 else:
                     patch = [*cells[numRow][numCol], brushColours[1], brushColours[1], 0, " "]
                 patches += [patch]
+        return patches
+
+    def onMouseEvent(self, atPoint, brushColours, brushPos, brushSize, canvas, keyModifiers, mapPoint, mouseDragging, mouseLeftDown, mouseRightDown):
+        brushColours = [brushColours[1], brushColours[0]] if mouseRightDown else brushColours
+        brushSize = [brushSize[0] * 2, brushSize[1]]
+        if self.toolState == self.TS_NONE:
+            originPoint, radius, targetPoint = list(mapPoint), brushSize[0], (brushSize[0] / 2, brushSize[0] / 2,)
+            if (keyModifiers == wx.MOD_CONTROL) and (mouseLeftDown or mouseRightDown):
+                self.brushColours, isCursor, self.originPoint, self.toolState = list(brushColours), True, originPoint, self.TS_ORIGIN
+            else:
+                isCursor = not (mouseLeftDown or mouseRightDown)
+        elif self.toolState == self.TS_ORIGIN:
+            brushColours, originPoint = self.brushColours, self.originPoint
+            if mapPoint[0] > self.originPoint[0]:
+                radius, targetPoint = brushSize[0] + (mapPoint[0] - self.originPoint[0]), ((brushSize[0] + (mapPoint[0] - self.originPoint[0])) / 2, (brushSize[0] + (mapPoint[0] - self.originPoint[0])) / 2,)
+            else:
+                radius, targetPoint = brushSize[0], (brushSize[0] / 2, brushSize[0] / 2,)
+            if keyModifiers != wx.MOD_CONTROL:
+                isCursor, self.brushColours, self.originPoint, self.toolState = False, None, None, self.TS_NONE
+            else:
+                isCursor = True
+        patches = self._drawCircle(brushColours, canvas, originPoint, targetPoint, radius)
         return True, patches if not isCursor else None, patches if isCursor else None
+
+    def __init__(self, *args):
+        super().__init__(*args); self.brushColours, self.originPoint, self.toolState = None, None, self.TS_NONE;
 
 # vim:expandtab foldmethod=marker sw=4 ts=4 tw=120
