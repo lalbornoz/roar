c279086c574b1e5a60b4ce29ab2a884226ea0799
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2019-09-27 14:22:58 +0200

    Tool bug fixes & usability improvements.
    1) Circle, rectangle tool: initiate dragging with <Ctrl>, conclude w/ <[LR]MB> release.
    2) Circle tool: fix artifacts and radius inference from brush size.
    3) Line tool: fix origin point artifacts.
    4) Rectangle tool: correctly determine target point.

--- libtools/ToolCircle.py
+++ libtools/ToolCircle.py
@@ -23,43 +23,43 @@ class ToolCircle(Tool):
                 del cells[-1]
         for numRow in range(len(cells)):
             for numCol in range(len(cells[numRow])):
-                if ((numRow == 0) or (numRow == (len(cells) - 1)))                              \
-                or ((numCol == 0) or (numCol == (len(cells[numRow]) - 1))):
-                    patch = [*cells[numRow][numCol], brushColours[0], brushColours[0], 0, " "]
-                elif ((numRow > 0) and (cells[numRow][numCol][0] < cells[numRow - 1][0][0]))    \
-                or   ((numRow < len(cells)) and (cells[numRow][numCol][0] < cells[numRow + 1][0][0])):
-                    patch = [*cells[numRow][numCol], brushColours[0], brushColours[0], 0, " "]
-                elif ((numRow > 0) and (cells[numRow][numCol][0] > cells[numRow - 1][-1][0]))   \
-                or   ((numRow < len(cells)) and (cells[numRow][numCol][0] > cells[numRow + 1][-1][0])):
-                    patch = [*cells[numRow][numCol], brushColours[0], brushColours[0], 0, " "]
-                elif brushColours[1] == -1:
-                    if  (cells[numRow][numCol][0] < canvas.size[0])                             \
-                    and (cells[numRow][numCol][1] < canvas.size[1]):
-                        patch = [cells[numRow][numCol][0], cells[numRow][numCol][1], *canvas.map[cells[numRow][numCol][1]][cells[numRow][numCol][0]]]
+                point = cells[numRow][numCol]
+                if  ((point[0] >= 0) and (point[1] >= 0))                                           \
+                and (point[0] < canvas.size[0]) and (point[1] < canvas.size[1]):
+                    if ((numRow == 0) or (numRow == (len(cells) - 1)))                              \
+                    or ((numCol == 0) or (numCol == (len(cells[numRow]) - 1))):
+                        patch = [*cells[numRow][numCol], brushColours[0], brushColours[0], 0, " "]
+                    elif ((numRow > 0) and (cells[numRow][numCol][0] < cells[numRow - 1][0][0]))    \
+                    or   ((numRow < len(cells)) and (cells[numRow][numCol][0] < cells[numRow + 1][0][0])):
+                        patch = [*cells[numRow][numCol], brushColours[0], brushColours[0], 0, " "]
+                    elif ((numRow > 0) and (cells[numRow][numCol][0] > cells[numRow - 1][-1][0]))   \
+                    or   ((numRow < len(cells)) and (cells[numRow][numCol][0] > cells[numRow + 1][-1][0])):
+                        patch = [*cells[numRow][numCol], brushColours[0], brushColours[0], 0, " "]
+                    elif brushColours[1] == -1:
+                        if  (cells[numRow][numCol][0] < canvas.size[0])                             \
+                        and (cells[numRow][numCol][1] < canvas.size[1]):
+                            patch = [cells[numRow][numCol][0], cells[numRow][numCol][1], *canvas.map[cells[numRow][numCol][1]][cells[numRow][numCol][0]]]
+                        else:
+                            patch = [*cells[numRow][numCol], brushColours[1], brushColours[1], 0, " "]
                     else:
                         patch = [*cells[numRow][numCol], brushColours[1], brushColours[1], 0, " "]
-                else:
-                    patch = [*cells[numRow][numCol], brushColours[1], brushColours[1], 0, " "]
-                patches += [patch]
+                    patches += [patch]
         return patches
 
     def onMouseEvent(self, atPoint, brushColours, brushPos, brushSize, canvas, keyModifiers, mapPoint, mouseDragging, mouseLeftDown, mouseRightDown):
-        brushColours = [brushColours[1], brushColours[0]] if mouseRightDown else brushColours
-        brushSize = [brushSize[0] * 2, brushSize[1]]
+        brushColours, brushSize = [brushColours[1], brushColours[0]] if mouseRightDown else brushColours, brushSize[0] * 2
         if self.toolState == self.TS_NONE:
-            originPoint, radius, targetPoint = list(mapPoint), brushSize[0], (brushSize[0] / 2, brushSize[0] / 2,)
+            originPoint, radius, targetPoint = list(mapPoint), brushSize, (brushSize / 2,) * 2
             if (keyModifiers == wx.MOD_CONTROL) and (mouseLeftDown or mouseRightDown):
-                self.brushColours, isCursor, self.originPoint, self.toolState = list(brushColours), True, originPoint, self.TS_ORIGIN
+                self.brushColours, isCursor, self.originPoint, self.toolState = brushColours, True, originPoint, self.TS_ORIGIN
             else:
                 isCursor = not (mouseLeftDown or mouseRightDown)
         elif self.toolState == self.TS_ORIGIN:
-            brushColours, originPoint = self.brushColours, self.originPoint
             if mapPoint[0] > self.originPoint[0]:
-                radius, targetPoint = brushSize[0] + (mapPoint[0] - self.originPoint[0]), ((brushSize[0] + (mapPoint[0] - self.originPoint[0])) / 2, (brushSize[0] + (mapPoint[0] - self.originPoint[0])) / 2,)
-            else:
-                radius, targetPoint = brushSize[0], (brushSize[0] / 2, brushSize[0] / 2,)
-            if keyModifiers != wx.MOD_CONTROL:
-                isCursor, self.brushColours, self.originPoint, self.toolState = False, None, None, self.TS_NONE
+                brushSize += (mapPoint[0] - self.originPoint[0]); brushSize = brushSize + (brushSize % 2);
+            brushColours, originPoint, radius, targetPoint = self.brushColours, self.originPoint, brushSize, (brushSize / 2,) * 2
+            if not (mouseLeftDown or mouseRightDown):
+                self.brushColours, isCursor, self.originPoint, self.toolState = None, False, None, self.TS_NONE
             else:
                 isCursor = True
         patches = self._drawCircle(brushColours, canvas, originPoint, targetPoint, radius)
